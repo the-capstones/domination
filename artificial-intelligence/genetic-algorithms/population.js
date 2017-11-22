e46d38a805c9683f2b78e65136ead52cec81f546
bfsilva713 <bfsilva@uci.edu>
2017-11-21 22:33:39 -0600

    beginning play function

--- artificial-intelligence/genetic-algorithms/population.js
+++ artificial-intelligence/genetic-algorithms/population.js
@@ -6,59 +6,93 @@ const trueskill = require('trueskill')
 
 function AIgenome() {
 
-    this.chanceToWinThreshold = Math.random()
-    this.playerStrengthQuotientThreshold =
-      Math.round(Math.random())
-        ? Math.random()
-        : null
-    this.attackStrategy =
-      Math.round(Math.random())
-        ? 'maximizeTerritoryGains'
-        : 'minimizeUnitsLostRatio'
-    this.allotmentStrategy =
-      Math.round(Math.random())
-        ? 'differenceInUnits'
-        : 'ratioOfUnits'
-    this.trueSkill = [25, 25/3]
+  this.chanceToWinThreshold = Math.random()
+  this.playerStrengthQuotientThreshold =
+    Math.round(Math.random())
+      ? Math.random()
+      : null
+  this.attackStrategy =
+    Math.round(Math.random())
+      ? 'maximizeTerritoryGains'
+      : 'minimizeUnitsLostRatio'
+  this.allotmentStrategy =
+    Math.round(Math.random())
+      ? 'differenceInUnits'
+      : 'ratioOfUnits'
+  this.skill = [25, 25 / 3]
+}
+
+function generateStartingGenomes() {
+  let genomes = []
+  for (let i = 0; i < 20; i++) {
+    genomes.push(new AIgenome())
+  }
+  return genomes;
+}
+
+let genomes = generateStartingGenomes()
+
+// console.log(genomes)
+
+/* genomes: array of AI players with random genetic material
+have these players play eachother (groups of 4)
+
+something like:
+  for(let i=0; i<20; i+=4) {
+    play(genomes[i], genomes[i+1], genomes[i+2], genomes[i+3])
   }
 
-  function generateStartingGenomes() {
-    let genomes = []
-    for (let i=0; i<20; i++) {
-      genomes.push(new AIgenome())
+(`play` function still has to be defined)
+(consider re-writing all AI functions as methods?)
+
+after gameplay, assign fitness property
+
+
+something like:
+  function play(player1, player2, player3, player4) {
+    let players = [...args]
+    let gameRank = players.length; //# of players
+
+    ...
+    if (${player} has no units left) {
+      player.rank = gameRank;
+      gameRank--
+      // somehow control for draws
     }
-    return genomes;
+    //when game ends
+    trueskill.AdjustPlayers(players)
+    //each player now has accurate 'trueskill' value as player.rank
   }
+*/
 
-  let genomes = generateStartingGenomes()
+function shufflePlayerOrder(playerArray) {
+  for (let i=0; i<playerArray.length; i++) {
+    let j=Math.floor(Math.random() * (playerArray.length))
+    let temp = playerArray[i]
+    playerArray[i] = playerArray[j]
+    playerArray[j] = temp
+  }
+  return playerArray
+}
 
-  // console.log(genomes)
+function play(player1, player2, player3, player4) {
+  let players = [...arguments]
+  let gameRank = players.length;
+  shufflePlayerOrder(players)
 
-  /* genomes: array of AI players with random genetic material
-  have these players play eachother (groups of 4)
-
-  something like:
-    for(let i=0; i<20; i+=4) {
-      play(genomes[i], genomes[i+1], genomes[i+2], genomes[i+3])
-    }
-
-  (`play` function still has to be defined)
-  (consider re-writing all AI functions as methods?)
-
-  after gameplay, assign fitness property
-
-
-  something like:
-    function play(player1, player2, player3, player4) {
-      let gameRank = arguments.length; //# of players
-      ...
-      if (${player} has no units left) {
-        player.gameRank = gameRank;
-        gameRank--
+  while (gameRank > 1) {
+    players.forEach(player, function takeTurn(player) {
+      //allotment logic
+      //battle logic
+      if (/* player has no units left */ !player.units /**/) {
+        player.rank = gameRank;
+        gameRank--;
       }
-    }
-  */
+      //fortification logic
+    })
+  }
+}
 
-  function getFitness(genomes) {
-
+function getFitness(genome) {
+  return genome.skill /
   }
