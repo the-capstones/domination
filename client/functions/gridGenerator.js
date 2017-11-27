9a0e902a0057427d8645e3eb7c3d72f8775ad2e3
Zachary Dudley Friedman <ZacharyDFriedman@gmail.com>
2017-11-27 11:04:34 -0600

    issue with Board importing config from gridGenerator file

--- client/functions/gridGenerator.js
+++ client/functions/gridGenerator.js
@@ -1,13 +1,14 @@
 import { GridGenerator } from 'react-hexgrid';
 import configs from '../configurations';
 
+export let config = configs['rectangle'];
+
 export const createGrid = (size) => {
-  const config = configs[size];
+  config = configs[size];
   const generator = GridGenerator.getGenerator(config.map);
   return generator.apply(this, config.mapProps);
 }
 
-export const config = configs['rectangle'];
 export const generator = GridGenerator.getGenerator(config.map);
 export const hexagons = generator.apply(this, config.mapProps);
 
