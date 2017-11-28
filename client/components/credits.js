a1735249695bb4809e949ac2d06093a56dfe7c21
Zachary Dudley Friedman <ZacharyDFriedman@gmail.com>
2017-11-28 15:04:47 -0600

    now with style

--- client/components/credits.js
+++ client/components/credits.js
@@ -8,20 +8,16 @@ const Credits = () => {
       <h1>About Us:</h1>
         <p>We're a rag-tag team of programmers fighting against all odds to bring you a legally permissable version of an existing strategy board game of diplomacy, conflict and conquest. Special thanks to <a href="https://www.fullstackacademy.com/">Fullstack Academy</a>.</p>
 
-      <h2>Zachary D Friedman</h2>
-        <h3><a href="https://zacharydfriedman.herokuapp.com/">Site</a></h3>
+      <h2>Zachary D Friedman  | <a href="https://zacharydfriedman.herokuapp.com/">Site</a></h2>
         <p>Zach is a programmer.</p>
 
-      <h2>Damian Michniak</h2>
-        <h3><a href="">Site</a></h3>
+      <h2>Damian Michniak  | <a href="">Site</a></h2>
         <p>Some words about Damian</p>
 
-      <h2>BreAnna Silva</h2>
-        <h3><a href="">Site</a></h3>
+      <h2>BreAnna Silva  | <a href="">Site</a></h2>
         <p>Some words about BreAnna</p>
 
-      <h2>Christian Sadi</h2>
-        <h3><a href="">Site</a></h3>
+      <h2>Christian Sadi  | <a href="">Site</a></h2>
         <p>Some words about Christian</p>
 
     </div>
