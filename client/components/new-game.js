50931160c0204cd2e75667a3371c1699a5001709
Zachary Dudley Friedman <ZacharyDFriedman@gmail.com>
2017-11-29 11:46:05 -0600

    reversed order in drop down

--- client/components/new-game.js
+++ client/components/new-game.js
@@ -62,16 +62,16 @@ const NewGame = (props) => {
           <div>
             <label htmlFor="allotmentRate"><small>Recieve an army for every </small></label>
             <select name="allotmentRate">
-              <option value={1} >1</option>
-              <option value={2} >2</option>
-              <option value={3} >3</option>
-              <option value={4} >4</option>
-              <option value={5} >5</option>
-              <option value={6} >6</option>
-              <option value={7} >7</option>
-              <option value={8} >8</option>
-              <option value={9} >9</option>
               <option value={10} >10</option>
+              <option value={9} >9</option>
+              <option value={8} >8</option>
+              <option value={7} >7</option>
+              <option value={5} >5</option>
+              <option value={4} >4</option>
+              <option value={3} >3</option>
+              <option value={6} >6</option>
+              <option value={2} >2</option>
+              <option value={1} >1</option>
             </select>
             <label><small> territories owned.</small></label>
           </div>
